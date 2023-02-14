d748f5a36f154680361abd4000aa2eb9f119999f
limory <50734875+limory@users.noreply.github.com>
2023-02-14 18:21:16 +0800

    Update opt-start.sh

--- trunk/user/optware/opt-start.sh
+++ trunk/user/optware/opt-start.sh
@@ -13,10 +13,10 @@ mountpoint -q /opt || exit 0
 # extend path to /opt
 export PATH=/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# 如果存在/opt/bin/opkg二进制文件， 则启动USB已安装的服务
+# 如果存在/opt/bin/opkg二进制文件， 则启动USB手动安装 需要自启动的服务/opt/etc/init.d下S***开头的服务
 # 如果不存在/opt/bin/opkg二进制文件，则启动安装sh
 if [ -f /opt/bin/opkg ] ; then
-	logger -t "${self_name}" "call /opt/etc/init.d" "启动USB已安装的服务"
+	logger -t "${self_name}" "call /opt/etc/init.d" "启动USB已安装的服务(S***开头的服务)"
 	# start all services S* in /opt/etc/init.d
 	for i in `ls /opt/etc/init.d/S??* 2>/dev/null` ; do
 		[ ! -x "${i}" ] && continue
