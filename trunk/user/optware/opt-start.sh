b8368e88ef2f8e90fcbaa2045fed7546caf83d09
limory <50734875+limory@users.noreply.github.com>
2023-02-14 14:18:07 +0800

    Update opt-start.sh

--- trunk/user/optware/opt-start.sh
+++ trunk/user/optware/opt-start.sh
@@ -3,38 +3,26 @@
 self_name="opt-start.sh"
 
 optw_enable=`nvram get optw_enable`
-[ "$optw_enable" != "1" -a "$optw_enable" != "2" ] && exit 0
+[ "$optw_enable" != "1" ] && exit 0
 
 # check /opt is mounted
+# 如果/opt已经挂载了 则继续执行下一步
+# 如果/opt没有挂载，则退出
 mountpoint -q /opt || exit 0
-
-logger -t "${self_name}" "call /opt/etc/init.d"
 
 # extend path to /opt
 export PATH=/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# start all services S* in /opt/etc/init.d
-for i in `ls /opt/etc/init.d/S??* 2>/dev/null` ; do
-	[ ! -x "${i}" ] && continue
-	${i} start
-done
-
-# install and update ipkg/opkg in background
-if [ "$optw_enable" == "1" ] ; then
-	if [ ! -f /opt/bin/ipkg ] ; then
-		if [ -f /opt/bin/opkg ] ; then
-			logger -t "Optware:" "WARNING! Entware detected (/opt). Please remove Entware first!"
-			exit 0
-		fi
-		/usr/bin/opt-ipkg-upd.sh
-	fi
-elif [ "$optw_enable" == "2" ] ; then
-	if [ ! -f /opt/bin/opkg ] ; then
-		if [ -f /opt/bin/ipkg ] ; then
-			logger -t "Entware:" "WARNING! Optware detected (/opt). Please remove Optware first!"
-			exit 0
-		fi
-		/usr/bin/opt-opkg-upd.sh
-	fi
+# 如果存在/opt/bin/opkg二进制文件， 则启动USB已安装的服务
+# 如果不存在/opt/bin/opkg二进制文件，则启动安装sh
+if [ -f /opt/bin/opkg ] ; then
+	logger -t "${self_name}" "call /opt/etc/init.d" "启动USB已安装的服务"
+	# start all services S* in /opt/etc/init.d
+	for i in `ls /opt/etc/init.d/S??* 2>/dev/null` ; do
+		[ ! -x "${i}" ] && continue
+		${i} start
+	done	
+	exit 0
+else
+	/usr/bin/opt-opkg-upd.sh
 fi
-
